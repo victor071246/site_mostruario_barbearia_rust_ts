24f538a2b0d65c12c11dc1a9cf272dbb503b0cea
v <daderiovictorfelipe@gmail.com>
2025-12-28 21:45:39 -0300

    Middlewares implementados no routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,21 +1,27 @@
 use axum::{
     routing::{get, post, patch, delete},
-    Router
+    Router,
+    middleware
 };
 use crate::handlers::*;
+use crate::auth::auth_middleware;
 
 pub fn create_routes(state: AppState) -> Router {
+
+    let public_routes = Router::new()
+    .route("/auth/register", post(register))
+    .route("/auth/login", post(login));
+
+    let protected_routes = Router::new()
+    .route("/items", get(list_items))
+    .route("/items", post(create_item))
+    .route("/items/:id", get(get_items))
+    .route("/items/:id", patch(update_item))
+    .route("/items/:id", delete(delete_items))
+    .layer(middleware::from_fn(auth_middleware));
+
     Router::new()
-        // Autenticação
-        .route("/auth/register", post(register))
-        .route("/auth/login", post(login))
-
-        // Items (CRUD completo)
-        .route("/items", get(list_items))
-        .route("/items", post(create_item))
-        .route("/items/:id", get(get_items))
-        .route("/items/:id", patch(update_item))
-        .route("/items/:id", delete(delete_items))
-
-        .with_state(state)
+    .merge(public_routes)
+    .merge(protected_routes)
+    .with_state(state)
 }
